86d2ff89709b4cc0e14bc9a49bc13074a2378379
Nektarios Ntontoulos <nekis.dodos@gmail.com>
2021-01-10 12:31:55 +0200

    update

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -64,7 +65,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert ('You continue');
+        // alert ('You continue');
+        const order = { 
+            ingredients: this.state.ingredients, // this is NOT a setup you would use on a production ready app. You would need to
+            price: this.state.totalPrice,         //  calculate the total price in the server, otherwise a user could manipulate the code
+            customer: {                         //\
+                name: 'Themis Dod',             // \
+                address: {                      //  \
+                    street: 'Teststreet 1',     //   \
+                    zipDode: 44444,             //    \
+                    country: 'Greece'           //      some dummy order datea
+                },                              //    /
+                email: 'test@test.com'          //   /
+            },                                  //  /
+            deliveryMethod: 'fastest'           // /
+        }
+        axios.post('/orders.json', order) // 'when using Firebase (the website we used for generating our URL) you need to add '.json' 
+            .then(response => console.log(response))
+            .catch (error => console.log(error));
     }
 
     render () {
